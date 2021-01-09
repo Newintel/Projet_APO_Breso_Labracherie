29b09a4f8cdf3ba2bfccfdda7d46cb15c8d35cb6
brsantoine <antoinebre@orange.fr>
2021-01-09 15:05:49 +0100

    Naissance de nouveaux individus, N=SEIR

--- ProjetEpidemie/src/classes/SimulationEpidemie.java
+++ ProjetEpidemie/src/classes/SimulationEpidemie.java
@@ -3,11 +3,11 @@ package classes;
 public class SimulationEpidemie {
 
 	public static void main(String[] args) {
-		Simulation simu = new Simulation(1, 32, 0.1, 1, 0.5, 4, 4);
+		Simulation simu = new Simulation(1, 32, 0.1, 1, 0.5, 1, 30, 4, 4);
 		System.out.println(simu.toString());
 		System.out.println(simu.getNbIndividus("contamine"));
 		System.out.println(simu.getNbIndividus("retire"));
-		simu.SEIR();
+		simu.SEIRN();
 		System.out.println(simu.toString());
 		System.out.println(simu.getNbIndividus("contamine"));
 		System.out.println(simu.getNbIndividus("retire"));
